c0eaa23226134537bc9fb1e7cb6e8baeebfa69b9
alexkov2710 <94488224+alexkov2710@users.noreply.github.com>
2022-12-18 12:34:16 +0300

    push to Github

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Textnote.Ru</h1>
+      <h1>Textnote.Ru!!! pushed to Github</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
