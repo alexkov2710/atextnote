df9985f57ea8d5996cdc5277f0462aacba9e17b9
alexkov2710 <94488224+alexkov2710@users.noreply.github.com>
2022-12-18 13:34:31 +0300

    рандомные заметки

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import moment from "moment";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([])
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Textnote.Ru!!! pushed to Github</h1>
+      </div> */}
+      <h1><span style={{ color: "chartreuse"}}>Text</span><span style={{ color: "mediumvioletred"}}>Note</span>.Ru</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className='note' onClick={() => setNotes((prev) => [
+            ...prev, 
+            moment().valueOf()
+          ])}>
+          Добавить заметку
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        {notes.length === 0 && <div><br/>Заметок пока нет...</div>}
+
+        {notes.length > 0 && notes.map(note => {
+          return (
+            <div key={note} className="note">
+              {note} 
+              &nbsp;&nbsp;&nbsp;
+              <button 
+                onClick={() => {
+                  setNotes((prev) => {
+                    const newNotes = prev.filter((n => n !== note));
+                    return newNotes
+                  })
+                }}>
+                Х
+              </button>
+            </div>
+          )
+        })}
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        TextNote.Ru @ 2022
       </p>
     </div>
   )
