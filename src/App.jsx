952ca0b7f938fe4f3c813ce3e79f6cad39a91590
alexkov2710 <94488224+alexkov2710@users.noreply.github.com>
2022-12-18 23:20:01 +0300

    подключена база на Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,57 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import moment from "moment";
+import localization from 'moment/locale/ru';
+import { createClient } from '@supabase/supabase-js'
+
+// moment.locale('ru', localization);
+moment.updateLocale('ru');
+
+const supabaseUrl = 'https://fqkdaqmmxvpkbrieowuv.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxa2RhcW1teHZwa2JyaWVvd3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEzNjc5MTEsImV4cCI6MTk4Njk0MzkxMX0.ZdLj4vCYQxTwNgqkCoOg3saSPYmSWKIT2B-h-R423Qw"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+let { data: notesData, error } = await supabase.from('notes').select('*');
+
+const addNote = async( noteRow ) => {
+  const { data, error } = await supabase
+  .from('notes')
+  .insert([
+    { title: noteRow.title },
+  ])
+  .select()
+  return data
+}
+
+const deleteNote = async( noteId ) => {
+  const { data, error } = await supabase
+  .from('notes')
+  .delete()
+  .eq('id', String(noteId) )
+  return data
+}
 
 function App() {
-  const [notes, setNotes] = useState([])
+
+  const [notes, setNotes] = useState(notesData)
 
   return (
     <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <h1><span style={{ color: "chartreuse"}}>Text</span><span style={{ color: "mediumvioletred"}}>Note</span>.Ru</h1>
       <div className="card">
 
-        <button className='add-note' onClick={() => setNotes((prev) => [
-            ...prev, 
-            moment().valueOf()
-          ])}>
+        <button className='add-note' onClick={() => {
+            addNote({}).then(response => {
+              if (!error) {
+                setNotes((prev) => [
+                  ...prev, 
+                  response[0]
+                ]);
+              } else {
+                alert("Ошибка удаления!");
+              }
+            })
+        }}>
           Добавить заметку
         </button>
 
@@ -30,15 +60,24 @@ function App() {
 
         {notes.length > 0 && notes.map(note => {
           return (
-            <div key={note} className="note">
-              {note} 
+            <div key={note.id} className="note">
+              <div><b>{moment(note.created_at).format(`DD.MM.YY hh:mm:ss`)} - [{note.id}]{note.title ? " - " + note.title : ""}</b></div>
+              <div>{note.note}</div>
               &nbsp;&nbsp;&nbsp;
               <button 
                 onClick={() => {
-                  setNotes((prev) => {
-                    const newNotes = prev.filter((n => n !== note));
-                    return newNotes
-                  })
+                  deleteNote( note.id ).then(
+                    error => {
+                      if (!error) {
+                        setNotes((prev) => {
+                          const newNotes = prev.filter((n => n.id !== note.id));
+                          return newNotes
+                        })
+                      } else {
+                        alert("Ошибка удаления!");
+                      }
+                    }
+                  )
                 }}>
                 Х
               </button>
